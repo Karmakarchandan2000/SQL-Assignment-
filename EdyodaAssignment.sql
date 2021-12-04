ea93fba4cccd9177c433b5f92b668e9eb5dd1083
Karmakarchandan2000 <92676110+Karmakarchandan2000@users.noreply.github.com>
2021-12-04 23:40:12 +0530

    SQL

---
+++ EdyodaAssignment.sql
@@ -0,0 +1,9 @@
+Create Database Edyoda_Assignment;
+use Edyoda_Assignment;
+create table SalesPeople(
+Snum int (5) PRIMARY KEY,
+Sname varchar(20) unique ,
+City varchar(15),
+Comm double(5,2) 
+);
+
